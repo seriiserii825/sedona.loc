ba62115123cc83122b9eda8cd202f5eb3d2d11dc
serii <seriiburduja@gmail.com>
2019-03-04 16:14:14 +0200

    + finish index.pug

--- gulpfile.js
+++ gulpfile.js
@@ -223,7 +223,6 @@ gulp.task('browser-sync', function () {
 gulp.task('default', gulp.series(
   'clean',
   gulp.parallel(
-    'pug',
     'css',
     'js',
     'alljs',
@@ -235,5 +234,5 @@ gulp.task('default', gulp.series(
     'libs',
     'favicon'
   ),
-  gulp.parallel('watch', 'browser-sync')
+  gulp.parallel('pug', 'watch', 'browser-sync')
 ));
